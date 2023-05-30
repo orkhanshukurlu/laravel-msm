1a77809c31251e59257b98e32deb20271933710e
Orkhan Shukurlu <orkhandev@gmail.com>
2023-05-30 23:17:52 +0400

    add getByCode method & add columns parameter

--- src/Models/MSMLog.php
+++ src/Models/MSMLog.php
@@ -45,16 +45,32 @@ final class MSMLog extends Model
     protected $table = 'msm_logs';
 
     /**
-     * Get all log data by the phone number.
+     * Get all log data by the response code.
      *
-     * @param int|string $phone
+     * @param int $code
+     * @param array $columns
      *
      * @return Collection
      */
-    public static function getByPhone(int|string $phone): Collection
+    public static function getByCode(int $code, array $columns = ['*']): Collection
+    {
+        return self::query()
+            ->where('response_code', $code)
+            ->get($columns);
+    }
+
+    /**
+     * Get all log data by the phone number.
+     *
+     * @param int|string $phone
+     * @param array $columns
+     *
+     * @return Collection
+     */
+    public static function getByPhone(int|string $phone, array $columns = ['*']): Collection
     {
         return self::query()
             ->where('phone', $phone)
-            ->get();
+            ->get($columns);
     }
 }
